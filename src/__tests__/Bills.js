4fd31d69abb10a9b0ac391eec7c39d9df936b5d0
Charles Cailleteau <charles.cailleteau@protonmail.com>
2021-11-03 21:06:17 +0100

    test get billjs et sumbit newbilljs

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,4 +1,4 @@
-import { fireEvent, screen, toHaveStyle } from '@testing-library/dom'
+import { fireEvent, screen } from '@testing-library/dom'
 import '@testing-library/jest-dom'
 import BillsUI from '../views/BillsUI.js'
 import { bills } from '../fixtures/bills.js'
@@ -7,6 +7,7 @@ import Firestore from '../app/Firestore'
 import { localStorageMock } from '../__mocks__/localStorage'
 import Bills from '../containers/Bills'
 import { ROUTES, ROUTES_PATH } from '../constants/routes.js'
+import firebase from '../__mocks__/firebase'
 
 describe('Given I am connected as an employee', () => {
 	describe('When I am on Bills Page', () => {
@@ -48,7 +49,7 @@ describe('Given I am connected as an employee', () => {
 			const onNavigate = pathname => {
 				document.body.innerHTML = ROUTES({ pathname })
 			}
-			const bills = new Bills({ document, onNavigate, firestore: null, localStorage: window.localStorage })
+			const bill = new Bills({ document, onNavigate, firestore: null, localStorage: window.localStorage })
 			$.fn.modal = jest.fn()
 			const eyeButton = screen.getAllByTestId('icon-eye')[0]
 			fireEvent.click(eyeButton)
@@ -67,6 +68,33 @@ describe('Given I am connected as an employee', () => {
 			const newBillBtn = screen.getByTestId('btn-new-bill')
 			fireEvent.click(newBillBtn)
 			expect(screen.getAllByText('Type de dépense')).toBeTruthy()
+		})
+	})
+	describe('When I am on Bills Page', () => {
+		const html = BillsUI({ data: [] })
+		document.body.innerHTML = html
+		const onNavigate = pathname => {
+			document.body.innerHTML = ROUTES({ pathname })
+		}
+		test('fetches bills from mock API GET', async () => {
+			const getSpy = jest.spyOn(firebase, 'get')
+			const bills = await firebase.get()
+			expect(getSpy).toHaveBeenCalledTimes(1)
+			expect(bills.data.length).toBe(4)
+		})
+		test('fetches bills from an API and fails with 404 message error', async () => {
+			firebase.get.mockImplementationOnce(() => Promise.reject(new Error('Erreur 404')))
+			const html = BillsUI({ error: 'Erreur 404' })
+			document.body.innerHTML = html
+			const message = await screen.getByText(/Erreur 404/)
+			expect(message).toBeTruthy()
+		})
+		test('fetches messages from an API and fails with 500 message error', async () => {
+			firebase.get.mockImplementationOnce(() => Promise.reject(new Error('Erreur 500')))
+			const html = BillsUI({ error: 'Erreur 500' })
+			document.body.innerHTML = html
+			const message = await screen.getByText(/Erreur 500/)
+			expect(message).toBeTruthy()
 		})
 	})
 })
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,14 +1,63 @@
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
+import { fireEvent, screen } from '@testing-library/dom'
+import NewBillUI from '../views/NewBillUI.js'
+import NewBill from '../containers/NewBill.js'
+import { localStorageMock } from '../__mocks__/localStorage'
+import { ROUTES, ROUTES_PATH } from '../constants/routes.js'
 
+describe('Given I am connected as an employee', () => {
+	describe('When I do fill fields in incorrect format and I click on button Send', () => {
+		test('Then it should renders New Bill page', () => {
+			const html = NewBillUI()
+			document.body.innerHTML = html
 
-describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
+			const type = screen.getByTestId('expense-type')
+			fireEvent.change(type, { target: { value: 'Services en ligne' } })
+			expect(type.value).toBe('Services en ligne')
+
+			const date = screen.getByTestId('datepicker')
+			fireEvent.change(date, { target: { value: '2021-11-01' } })
+			expect(date.value).toBe('2021-11-01')
+
+			const amountInput = screen.getByTestId('amount')
+			fireEvent.change(amountInput, { target: { value: '123' } })
+			expect(amountInput.value).toBe('123')
+
+			const tva = screen.getByTestId('vat')
+			fireEvent.change(tva, { target: { value: '10' } })
+			expect(tva.value).toBe('10')
+
+			const pct = screen.getByTestId('pct')
+			fireEvent.change(pct, { target: { value: '20' } })
+			expect(pct.value).toBe('20')
+
+			const file = screen.getByTestId('file')
+			fireEvent.change(file, { target: { value: '' } })
+			expect(file.value).toBe('')
+
+			const submitNewBill = screen.getByTestId('form-new-bill')
+			Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+			window.localStorage.setItem(
+				'user',
+				JSON.stringify({
+					type: 'Employee',
+				})
+			)
+
+			const onNavigate = pathname => {
+				document.body.innerHTML = ROUTES({ pathname })
+			}
+			const firestore = null
+			const newBill = new NewBill({
+				document,
+				onNavigate,
+				firestore,
+				localStorage: window.localStorage,
+			})
+			const handleSubmit = jest.fn(newBill.handleSubmit)
+			submitNewBill.addEventListener('submit', handleSubmit)
+			fireEvent.submit(submitNewBill)
+			expect(handleSubmit).toHaveBeenCalled()
+			expect(screen.getAllByText('Mes notes de frais')).toBeTruthy()
+		})
+	})
 })
